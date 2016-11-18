6547f8f2c304cc5b4ec01401b97e7bd74b672705
nfranchetti <nick.franchetti@gmail.com>
2016-11-18 12:06:44 -0600

    Added type parser

--- week08/parser.py
+++ week08/parser.py
@@ -9,6 +9,9 @@ def extract_base(json_blob, json_key):
 def stats_list(list_of_data):
 	return [(item['stat']['name'], item['base_stat']) for item in list_of_data]
 
+def types_list(list_of_data):
+	return [(item['slot'], item['type']['name']) for item in list_of_data]
+
 base_experience = extract_base(json_data, 'base_experience')
 height = extract_base(json_data, 'height')
 weight = extract_base(json_data, 'weight')
@@ -19,3 +22,6 @@ print 'Its height is %s' % height
 print 'Its weight is %s' % weight
 for item in base_stats:
 	print 'Bulby\'s %s starts at %s' % (item[0], item[1])
+
+for item in types_list(extract_base(json_data, 'types')):
+	print 'Bulby\'s type %s is %s' % (item[0], item[1])
