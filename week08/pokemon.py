ba6824cfe5212b174307b83ee5414f609b2d9021
nfranchetti <nick.franchetti@gmail.com>
2016-11-18 11:20:17 -0600

    Parser gets info for one pokemon

--- week08/pokemon.py
+++ week08/pokemon.py
@@ -1,3 +1,12 @@
 import requests as r
 import json
 
+BASE_URL = 'http://pokeapi.co/api/v2/'
+
+def get_pokemon_data(pokenumber):
+	request_url = BASE_URL + 'pokemon/%s' % pokenumber
+	pokedata = r.get(request_url)
+	return pokedata.json()
+
+with open('bulbasaur.txt', 'w') as outfile:
+	json.dump(get_pokemon_data(1), outfile)
